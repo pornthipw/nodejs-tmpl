b60316a547cffc8909350f90b7844f4f24261883
unknown <pornthip.wong@gmail.com>
2012-11-10 09:27:34 +0700

    edit bug save to db

--- server.js
+++ server.js
@@ -85,25 +85,25 @@ app.param('db', function(req, res, next) {
     } else {
       var required_authen = false;
       for(var idx in config.mongodb.auth) {
-	if(config.mongodb.auth[idx].name == req.params.db) {
-	  required_authen = true;
-	  db.authenticate(config.mongodb.auth[idx].username, config.mongodb.auth[idx].password, function(err,result) {
-	    if(err) {
-	      res.send(500, err);
-	    } else {
-	      if(result) {
-		req.db = db;
-		next();
-	      } else {
-		res.send(403, 'Unauthorized');
-	      }
-	    }
-	  });
-	}
+        if(config.mongodb.auth[idx].name == req.params.db) {
+          required_authen = true;
+          db.authenticate(config.mongodb.auth[idx].username, config.mongodb.auth[idx].password, function(err,result) {
+            if(err) {
+              res.send(500, err);
+            } else {
+              if(result) {
+                req.db = db;
+                next();
+              } else {
+                res.send(403, 'Unauthorized');
+              }
+            }
+          });
+        }
       }
       if(!required_authen) {
-	req.db = db;
-	next();
+        req.db = db;
+        next();
       }
     }
   });
@@ -135,23 +135,23 @@ app.post('/:db/files/:id', function (req,res) {
   var fileId = db.bson_serializer.ObjectID.createFromHexString(req.params.id);
   console.log(fileId);   
   mongodb.GridStore.exist(db, fileId, function(err, exist) {                
-      if(exist) {
-	var gridStore = new mongodb.GridStore(db, fileId, 'w');
-	  gridStore.open(function(err, gridStore) {
-	  //gridStore.contentType = req.query.contentType;
-	  console.log(req.body.content);
-	  gridStore.write(new Buffer(req.body.content, "utf8"), function(err, gridStore) {                    
-	    if(!err) {
-	      gridStore.close(function(err, result) {                        
-		if(!err) {
-		  res.json(result);
-		  db.close();
-		}
-	      });
-	    }
-	  }); 
-	});
-      } 
+    if(exist) {
+      var gridStore = new mongodb.GridStore(db, fileId, 'w');
+        gridStore.open(function(err, gridStore) {
+        //gridStore.contentType = req.query.contentType;
+        console.log(req.body.content);
+        gridStore.write(new Buffer(req.body.content, "utf8"), function(err, gridStore) {                    
+          if(!err) {
+            gridStore.close(function(err, result) {                        
+              if(!err) {
+                res.json(result);
+                db.close();
+              }
+            });
+          }
+        }); 
+      });
+    } 
   });
 });
 
@@ -196,17 +196,17 @@ app.post('/:db/files', function (req,res){
     var gridStore = new mongodb.GridStore(db, new mongodb.ObjectID(),'New File', 'w');   
     gridStore.open(function(err, gridStore) {
       gridStore.write(new Buffer(req.body.content, "utf8"),function(err, response) { 
-	if(err) {          
+        if(err) { 
           res.send(JSON.stringify({success:false,message:err}));              
         }
-	gridStore.close(function(err, result) {
+        gridStore.close(function(err, result) {
           if(err) {            
             res.send(JSON.stringify({success:false,message:err}));              
           }
           console.log(JSON.stringify(result));
           res.send(JSON.stringify({success:true, response:result})); 	  
-	  db.close();
-	});
+          db.close();
+        });
       });
     });    
   }
@@ -228,23 +228,23 @@ app.get('/:db/files/:id', function (req,res){
       var context = {};
                         
       gs.seek(0, function() {
-	gs.read(function(err, data) {	    	    
-	  if(!err) {
-	    context ={success:true,content:data.toString('utf8')}; 	    
-	    if (fields && fields.indexOf("document") != -1) {
-	      gs.collection(function(err, collection) {		
-		collection.findOne({_id:fileId}, function(err, doc) {
-		  context['document'] = doc;
-		  res.json(context);
-		});
-	      });      		      
-	    } else {
-	      res.json(context);
-	    }	          
-	  } else {
-	    res.json({success:false});
-	  }	  
-	});
+        gs.read(function(err, data) {	    	    
+          if(!err) {
+            context ={success:true,content:data.toString('utf8')}; 	    
+            if (fields && fields.indexOf("document") != -1) {
+              gs.collection(function(err, collection) {		
+                collection.findOne({_id:fileId}, function(err, doc) {
+                  context['document'] = doc;
+                  res.json(context);
+                });
+              });      		      
+            } else {
+              res.json(context);
+            }	          
+          } else {
+            res.json({success:false});
+          }	  
+        });
       });
       
       
@@ -257,26 +257,26 @@ app.delete('/:db/files/:id',function(req,res){
   var db = req.db;
   if (req.params.id.length == 24) {
     try {
-	fileId = new mongodb.ObjectID.createFromHexString(req.params.id);
-	mongodb.GridStore.exist(db, fileId, function(err, exist) {   
-	    if(exist) {
-		var gridStore = new mongodb.GridStore(db, fileId, 'w');
-		gridStore.open(function(err, gs) {                        
-		    gs.unlink(function(err, result) { 
-			if(!err) {                              
-			    res.json({'response':req.params.id}); 
-			    //client.close();                                        
-			} else {
-			    console.log(err);
-			}
-		    });                        
-		});//gridStore.open()
-	    } else {
-		console.log(id +' does not exists');
-	    }
-	});
+      fileId = new mongodb.ObjectID.createFromHexString(req.params.id);
+      mongodb.GridStore.exist(db, fileId, function(err, exist) {   
+          if(exist) {
+            var gridStore = new mongodb.GridStore(db, fileId, 'w');
+            gridStore.open(function(err, gs) {                        
+                gs.unlink(function(err, result) { 
+              if(!err) {                              
+                  res.json({'response':req.params.id}); 
+                  //client.close();                                        
+              } else {
+                  console.log(err);
+              }
+                });                        
+            });//gridStore.open()
+          } else {
+              console.log(id +' does not exists');
+          }
+      });
     } catch (err) {
-	console.log(err);
+      console.log(err);
     }
   }
 });
